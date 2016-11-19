fda92e57b98f79add8921fb8a749fd11dd5406cb
aliendude <pjrodriguezg@unal.edu.co>
2016-11-19 10:11:01 -0500

    Calculating midi notes start and ending samples
    Signed-off-by: aliendude <pjrodriguezg@unal.edu.co>

--- autotune.m
+++ autotune.m
@@ -1,11 +1,10 @@
 %[filename, pathname] = uigetfile('.mid', 'Seleccione el archivo midi');
 %midi = readmidi(strcat(pathname,filename));
 midi = readmidi('midi/base_de_prueba.mid');
-[audio_y, Fs] = audioread('audio/rec.m4a');
-[lenght_audio, a] = size(audio_y);
+[audio_y, audio_Fs] = audioread('audio/rec.m4a');
+[audio_length, a] = size(audio_y);
 disp('Audio length (s):');
-disp(lenght_audio/Fs);
-%new_y = 
+disp(audio_length/audio_Fs);
 % sound(y,Fs);
 % synthesize with FM-synthesis.
 % (y = audio samples.  Fs = sample rate.  Here, uses default 44.1k.)
@@ -35,6 +34,7 @@ ylabel('note number');
 [rows, columns] = size(notes);
 
 all_notes = [];
+new_y = [];
 for i = 1:rows
     note_properties = [ ];
 	for j = 1:columns
@@ -54,11 +54,26 @@ for i = 1:rows
             %append(note_properties,notes(i,j));
             note_properties = [note_properties notes(i,j)*2];% *2 just to make it longer
         end;
-	end;
+    end
+%     disp('..')
+%     disp(audio_length)
+%     disp('..')
+%     disp(audio_Fs)
+%     disp('..')
+%     disp(note_properties(2))
+    sample_init = audio_Fs* note_properties(2);
+    sample_end = audio_Fs* note_properties(3);
+    %disp(sample_init);
+    %disp(note_properties(2));
+    % Create sub array with portion of audio according to the midi note
+    % time
+    %new_y = audio_y(sample_init:sample_end);
+    
     all_notes = [all_notes note_properties];
     %disp(all_notes);
-	disp('---------');
+	%disp('---------');
 end;
+sound(new_y,Fs);
 % for i = 1:rows
 %     for j =1:3
 %         disp(all_notes(i,j));
